793b0d92ed116922e60dd061efc1b692311ad14b
KhanhNam17 <cunamkhanh1709@gmail.com>
2026-04-06 09:57:54 +0700

    Refactor CartItem component to include quantity handlers

--- CartItem.jsx
+++ CartItem.jsx
@@ -1,11 +1,62 @@
-const handleIncrement = (item) => {
-  dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeItem, updateQuantity } from './CartSlice';
+import './CartItem.css';
+
+const CartItem = ({ onContinueShopping }) => {
+  const cartItems = useSelector(state => state.cart.items);
+  const dispatch = useDispatch();
+
+  // Tính tổng tiền của cả giỏ hàng
+  const calculateTotalAmount = () => {
+    return cartItems.reduce((total, item) => total + (parseFloat(item.cost.substring(1)) * item.quantity), 0);
+  };
+
+  // NƠI ĐẶT 2 HÀM BẠN VỪA GỬI
+  const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
+  };
+
+  const handleDecrement = (item) => {
+    if (item.quantity > 1) {
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    } else {
+      dispatch(removeItem(item.name));
+    }
+  };
+
+  return (
+    <div className="cart-container">
+      <h2>Total Cart Amount: ${calculateTotalAmount()}</h2>
+      
+      <div>
+        {cartItems.map(item => (
+          <div className="cart-item" key={item.name}>
+            <img className="cart-item-image" src={item.image} alt={item.name} />
+            <div className="cart-item-details">
+              <div className="cart-item-name">{item.name}</div>
+              <div className="cart-item-cost">{item.cost}</div>
+              
+              <div className="cart-item-quantity">
+                {/* Sử dụng hàm ở đây */}
+                <button className="cart-item-button" onClick={() => handleDecrement(item)}>-</button>
+                <span className="cart-item-quantity-value">{item.quantity}</span>
+                <button className="cart-item-button" onClick={() => handleIncrement(item)}>+</button>
+              </div>
+              
+              {/* Nút xóa */}
+              <button className="cart-item-delete" onClick={() => dispatch(removeItem(item.name))}>Delete</button>
+            </div>
+          </div>
+        ))}
+      </div>
+      
+      <div className="continue_shopping_btn">
+        <button className="get-started-button" onClick={(e) => onContinueShopping(e)}>Continue Shopping</button>
+        <button className="get-started-button1" onClick={() => alert('Coming Soon')}>Checkout</button>
+      </div>
+    </div>
+  );
 };
 
-const handleDecrement = (item) => {
-  if (item.quantity > 1) {
-    dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
-  } else {
-    dispatch(removeItem(item.name));
-  }
-};
+export default CartItem;
