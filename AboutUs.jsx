8c29d3688535597d01d79111ec1acce9da2a3731
KhanhNam17 <cunamkhanh1709@gmail.com>
2026-04-06 09:47:07 +0700

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div className="about-us-container">
+      <h1>About Paradise Nursery</h1>
+      <p>Chào mừng bạn đến với Paradise Nursery, nơi cung cấp những loại cây cảnh tuyệt vời nhất cho không gian sống của bạn.</p>
+    </div>
+  );
+};
+
+export default AboutUs;
